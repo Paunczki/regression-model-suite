c919c3482d484ff005a78258ceb9c3669c2d7cb0
Ashton Hansen <ashhansen6@gmail.com>
2019-07-24 18:28:05 -0500

    1-var-linreg out of context

---
+++ 1-var-linreg-basic.R
@@ -0,0 +1,4 @@
+# You will need to download the data and fill in your own file path for read.csv()
+# In this file, "sample 'x'" will be used as a substitute
+df1 <- read.csv("sample.csv",
+                header = TRUE)
