89a58e78f351eb25cd547f49abfd71b0f64d030a
Ashton Hansen <ashhansen6@gmail.com>
2019-07-29 17:12:45 -0500

    Added two new sections

--- model-selector.R
+++ model-selector.R
@@ -2,8 +2,27 @@ FiveVar <- read.csv("C:/Users/Ashton Hansen/Documents/GitHub/regression-model-su
                  header= T)
 
 # This is a collection of different methods that can be used to build, compare, and select predictive models.
-# It should be remembered that different methods have pros/cons and that 
+# It should be remembered that different methods have pros/cons and that the method used depends no the 
+# situation.
 
+# CONTENTS:
+# - Manual Selection, p-value based
+# - Forward AIC Model using MASS Package
+
+#####################################
+## Manual Selection, p-value based ##
+#####################################
+# For smaller models, it may be easier to manually create the model. The selection process can be based off
+# of several different statistics, and predictor p-values are one type of these statistics.
+library("car")
+fit1 <- lm(Y ~., data= FiveVar) # 'Y~.' builds a model using all other variables (interaction terms excluded)
+summary(fit1)
+Anova(fit1, type= "III") # Use Anova() from the 'car' package to use type III sums of squares
+
+#######################################
+## Manual Selection, R^2 value based ##
+#######################################
+# Models can also be built based on model R^2 values.
 
 ##########################################
 ## Forward AIC Model using MASS Package ##
@@ -16,13 +35,13 @@ FiveVar <- read.csv("C:/Users/Ashton Hansen/Documents/GitHub/regression-model-su
 # is different from the backward stepwise model in that the forward model begins with a null model
 # with no predictors and the backward model starts with all predictors included.
 
-library(MASS)
-fit = lm(FiveVar$Y~1) # Create the null model first
-FiveVarFit1 <- stepAIC(fit, FiveVar$Y ~ FiveVar$X1 * FiveVar$X2 * FiveVar$X3 * FiveVar$X4 * FiveVar$X5, direction= "forward")
+library(MASS) # Needed for stepAIC()
+null = lm(FiveVar$Y~1) # Create the null model first
+FiveVarFit3 <- stepAIC(null, FiveVar$Y ~ FiveVar$X1 * FiveVar$X2 * FiveVar$X3 * FiveVar$X4 * FiveVar$X5, direction= "forward")
 # Notice that '*' is used instead of '+' for writing the model. Since stepAIC() can do a good amount
 # of work relatively quickly to manually building a model, we can include interaction terms without too
 # much worry about the time spent building models.
-FiveVarFit1
+FiveVarFit3
 
 # Benefits of using stepAIC():
 # - Fast and convenient
