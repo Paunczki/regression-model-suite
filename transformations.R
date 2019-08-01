0ce005b477e75a789a1cde77d4edc927f615e86c
Ashton Hansen <ashhansen6@gmail.com>
2019-08-01 16:52:28 -0500

    Adapted file for 3Var data

--- transformations.R
+++ transformations.R
@@ -5,24 +5,30 @@
 FiveVar <- read.table("C:/Users/Ashton Hansen/Documents/GitHub/regression-model-suite/data/5var.csv",
                         sep= ",",
                         header= T)
-FiveVarFitX2 <- lm(Y~X2, data= FiveVar)
-FiveVarFitX3 <- lm(Y~X3, data= FiveVar)
-FiveVarFitX4 <- lm(Y~X4, data= FiveVar)
 
 # FiveVarFitX2
+FiveVarFitX2 <- lm(Y~X2, data= FiveVar)
+
 plot(Y ~ X2, data= FiveVar,
-     main= "Y Regressed onto X2",
+     main= "5var: Y vs X2",
      ylab= "Y",
      xlab= "X2")
 abline(reg = FiveVarFitX2,
        col= "red")
-plot(lm(Y ~ X2, data= FiveVar), 1)
 
-# FiveVarFitX3
-plot(Y ~ X3, data= FiveVar,
-     main= "Y Regressed onto X3",
+plot(lm(Y ~ X2, data= FiveVar), 1) # The relationship between X2 and Y look similar, no transformation needed.
+
+# ThreeVarFitX2
+ThreeVar <- read.table("C:/Users/Ashton Hansen/Documents/GitHub/regression-model-suite/data/3var.csv",
+                       sep= ",",
+                       header= T)
+ThreeVarFitX1 <- lm(Y~X1, data= ThreeVar)
+
+plot(Y ~ X1, data= ThreeVar,
+     main= "3var: Y vs X1",
      ylab= "Y",
-     xlab= "X3")
-abline(reg = FiveVarFitX3,
+     xlab= "X1")
+abline(reg = ThreeVarFitX3,
        col= "red")
-plot(lm(Y ~ X3, data= FiveVar), 1)
+
+plot(lm(Y ~ X1, data= ThreeVar), 1) # The relationship between X1 and Y look logarithmic and will need to be transformed.
